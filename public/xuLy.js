b687e835992017f9ba529c1c68a54fa96186a4ca
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-24 14:41:14 +0700

    form

--- public/xuLy.js
+++ public/xuLy.js
@@ -1,9 +1,16 @@
 const socket = io();
 socket.on('SERVER_SEND_MESSAGE', value => console.log(value));
 
-$('#btnSend').click(() => {
+// $('#btnSend').click(() => {
+//     const msg = $('#txtMessage').val();
+//     $('#txtMessage').val('')
+//     socket.emit('CLIENT_SEND_MESSAGE', msg);
+// });
+
+function sendText(event) {
+    event.preventDefault();
     const msg = $('#txtMessage').val();
     $('#txtMessage').val('')
     socket.emit('CLIENT_SEND_MESSAGE', msg);
-});
+}
 
