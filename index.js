8a6f3e28de5c72a8525317ad7e8ff4a963c96522
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-24 13:59:20 +0700

    setup socket

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -8,4 +11,8 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
 
-app.listen(3000, () => console.log('Server started!'));
+server.listen(3000, () => console.log('Server started!'));
+
+io.on('connection', socket => {
+    console.log(socket.id);
+});
