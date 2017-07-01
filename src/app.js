d963dc5d3f6664b74cebff171bb5fad1c58c78ff
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-01 10:36:22 +0700

    xong

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 const io = require('socket.io-client');
 const $ = require('jquery');
+
 const socket = io();
 let idRemote;
 
 socket.on('SERVER_SEND_MESSAGE', value => {
-    $('#listMessage').append(`<li>${value}</li>`)
+    $('#listMessage').append(`<li>${value}</li>`);
 });
 
 socket.on('SERVER_SEND_PRIVATE_MESSAGE', value => {
-    $('#listMessage').append(`<li>${value}</li>`)
+    $('#listMessage').append(`<li>${value}</li>`);
 });
 
-socket.on('SERVER_REFUSE', () => alert('Please choose another username'));
+socket.on('SERVER_REFUSE', () => alert('Please choose another username')); // eslint-disable-line
 socket.on('SERVER_ACCEPT', arrUser => {
     $('#div-chat').show();
     $('#div-sign-up').hide();
     arrUser.forEach(e => $('#listUser').append(`<li id="user-${e.id}">${e.username}</li>`));
-    socket.on('NEW_USER', user => $('#listUser').append(`<li id="user-${user.id}">${user.username}</li>`));
+    socket.on('NEW_USER', user => {
+        const toAppend = `<li id="user-${user.id}">${user.username}</li>`;
+        $('#listUser').append(toAppend);
+    });
 });
 
 socket.on('USER_DISCONNECT', socketId => {
@@ -25,10 +29,10 @@ socket.on('USER_DISCONNECT', socketId => {
 
 $('#div-chat').hide();
 
-function sendText(event) {
+function sendText(event) { // eslint-disable-line
     event.preventDefault();
     const msg = $('#txtMessage').val();
-    $('#txtMessage').val('')
+    $('#txtMessage').val('');
     socket.emit('CLIENT_SEND_MESSAGE', msg);
 }
 
@@ -37,7 +41,7 @@ $('#btnSignUp').click(() => {
     socket.emit('CLIENT_SIGN_UP', username);
 });
 
-$('#listUser').on('click', 'li', function() {
+$('#listUser').on('click', 'li', function () {
     $('#listUser li').removeClass('active');
     $(this).addClass('active');
     const idAttr = $(this).attr('id');
@@ -54,7 +58,7 @@ $('#btnSendRoom').click(() => {
     socket.emit('ROOM_MESSAGE', msg);
 });
 
-$('.li-room').click(function() {
+$('.li-room').click(function () {
     const idRoom = $(this).attr('id');
     $('.li-room').removeClass('activeRoom');
     $(this).addClass('activeRoom');
